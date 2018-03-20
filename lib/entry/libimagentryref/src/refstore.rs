5284b56a61674ccacdd086403ccf65e7230ba929
Matthias Beyer <mail@beyermatthias.de>
2018-03-20 16:43:27 +0100

    Fix: Passed wrong path

--- lib/entry/libimagentryref/src/refstore.rs
+++ lib/entry/libimagentryref/src/refstore.rs
@@ -103,6 +103,7 @@ impl<'a> RefStore<'a> for Store {
     fn create_ref<RPG: UniqueRefPathGenerator, A: AsRef<Path>>(&'a self, path: A)
         -> Result<FileLockEntry<'a>, RPG::Error>
     {
+        let filepath = path.as_ref().to_path_buf();
         let hash     = RPG::unique_hash(path)?;
         let pathbuf  = PathBuf::from(format!("{}/{}", RPG::collection(), hash));
         let sid      = StoreId::new_baseless(pathbuf.clone()).map_err(RE::from)?;
@@ -111,7 +112,7 @@ impl<'a> RefStore<'a> for Store {
         self.create(sid)
             .map_err(RE::from)
             .and_then(|mut fle| {
-                fle.make_ref(hash, pathbuf)?;
+                fle.make_ref(hash, filepath)?;
                 Ok(fle)
             })
             .map_err(RPG::Error::from)
